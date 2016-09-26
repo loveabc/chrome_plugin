df8b1ea2ecb0796c263f5c75394f7694f6daa593
company <zhangwenquan@cathaylife.cn>
2016-09-26 17:41:40 +0800

    谷歌插件

---
+++ baidu.js
@@ -0,0 +1,4 @@
+//imageID=s_lg_img
+
+$("#s_lg_img").attr("src","http://docs.ebdoor.com/Image/ProductImage/0/2102/21027267_1.JPG")
+$("#su").val("谷歌搜索")
---
+++ crm.js
@@ -0,0 +1,10 @@
+$("input[type='button']").click(function(){
+
+	var type=$("select[name='type']").val()
+	var key=$("input[name='code_name']").val()
+	if(type=="ins"||type=="del"||type=="update"){
+		alert(key)
+	}
+	
+
+});
