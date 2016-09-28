05a3e217b83a2f1e7a46d56768e67085e2a8f104
company <zhangwenquan@cathaylife.cn>
2016-09-28 08:33:22 +0800

    每次新增,修改,删除SQLKEY,都会保存key值到localStorage,仅chrome测试

--- baidu.js
+++ baidu.js
@@ -2,3 +2,5 @@
 
 $("#s_lg_img").attr("src","http://docs.ebdoor.com/Image/ProductImage/0/2102/21027267_1.JPG")
 $("#su").val("谷歌搜索")
+localStorage.setItem("baidu","百度");
+localStorage.setItem("google","谷歌");
--- crm.js
+++ crm.js
@@ -3,7 +3,7 @@ $("input[type='button']").click(function(){
 	var type=$("select[name='type']").val()
 	var key=$("input[name='code_name']").val()
 	if(type=="ins"||type=="del"||type=="update"){
-		alert(key)
+		localStorage.setItem(key,key);
 	}
 	
 
